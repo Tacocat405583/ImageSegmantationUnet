c8feef7830e34899db25cac222c3f93376ae5d3d
unknown <nicolashernan2029@gmail.com>
2026-06-11 01:47:39 -0700

    Implemented loss function fix

--- dataset.py
+++ dataset.py
@@ -1,11 +1,19 @@
 ##LETS LOAD THE DATASET
 import torch
 from torchvision.datasets import OxfordIIITPet
+from torchvision import transforms
+from PIL import Image
+import numpy as np
 
-class MyDataSet(torch.utils.datasets):
+
+class MyDataSet(torch.utils.data.Dataset):
     def __init__(self,root='./data',split='trainval',size=256):
         self.ds = OxfordIIITPet(root=root,split=split,
                                 target_types='segmentation',download=True)
+        
+        self.size = size
+
+        self.to_tensor = transforms.ToTensor() 
 
     def __len__(self):
         return len(self.ds)
@@ -13,9 +21,26 @@ class MyDataSet(torch.utils.datasets):
     def __getitem__(self, key):
         img,mask = self.ds[key]
 
+        img = img.resize((self.size,self.size))
+        img = self.to_tensor(img)
+
+        mask = mask.resize((self.size,self.size),Image.Resampling.NEAREST)
+        mask = np.array(mask)
+        mask = torch.from_numpy(mask)
+
+        #print(mask.size())
+
+        mask = mask.long() - 1 #long for loss function
+        
+
         return img,mask
 
 
-
+if __name__ == "__main__":
+    ds = MyDataSet()
+    img,mask = ds[0]
+    print(img.shape,img.dtype)
+    print(mask.shape,mask.dtype)
+    print(mask.unique())
 
 
