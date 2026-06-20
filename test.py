e84bde3de8c74e30f9d19e6777c47e93cdda438c
unknown <nicolashernan2029@gmail.com>
2026-06-19 21:40:28 -0700

    Added visualize method
    Shows the models segmentation result for 10 images

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from torch.utils.data.dataloader import DataLoader
 from dataset import MyDataSet
 from model import UNET
 import torch.nn as nn
+import matplotlib.pyplot as plt
 
 # --- CONFIG -- 
 NUM_CLASSES = 3
@@ -45,8 +46,45 @@ def test():
 
 
 
+def visualize(num_samples=10):
+    test_ds = MyDataSet(split='test', size=IMG_SIZE)
+
+    model = UNET(in_channels=3, out_channels=NUM_CLASSES).to(DEVICE)
+    model.load_state_dict(torch.load('unet.pth'))
+    model.eval()
+
+    mean = torch.tensor([.485, .456, .406]).view(3,1,1)
+    std  = torch.tensor([.229, .224, .225]).view(3,1,1)
+
+    _, axes = plt.subplots(num_samples, 3, figsize=(10, num_samples * 3))
+    axes[0, 0].set_title('Image')
+    axes[0, 1].set_title('Ground Truth')
+    axes[0, 2].set_title('Prediction')
+
+    with torch.no_grad():
+        for i in range(num_samples):
+            img, mask = test_ds[i]
+
+            pred = model(img.unsqueeze(0).to(DEVICE))
+            pred_mask = pred.argmax(dim=1).squeeze(0).cpu()
+
+            display_img = (img * std + mean).permute(1, 2, 0).clamp(0, 1)
+
+            axes[i, 0].imshow(display_img)
+            axes[i, 1].imshow(mask, vmin=0, vmax=2)
+            axes[i, 2].imshow(pred_mask, vmin=0, vmax=2)
+
+            for ax in axes[i]:
+                ax.axis('off')
+
+    plt.tight_layout()
+    plt.savefig('predictions.png')
+    print("Saved to predictions.png")
+
+
 if __name__ == "__main__":
-    test()
+    #test()
+    visualize()
 
 
 
